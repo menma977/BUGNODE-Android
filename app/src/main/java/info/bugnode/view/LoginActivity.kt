6ad8d1f4accd2d29b23913571247d12c1a213f0c
SusuTawar <realsusutawar@gmail.com>
2020-10-29 00:23:58 +0700

    login

--- app/src/main/java/info/bugnode/view/LoginActivity.kt
+++ app/src/main/java/info/bugnode/view/LoginActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import org.json.JSONObject
 import info.bugnode.R
 import info.bugnode.config.Loading
+import info.bugnode.controller.WebController
+import okhttp3.FormBody
 import java.util.*
 import kotlin.concurrent.schedule
 
 class LoginActivity : AppCompatActivity() {
   private lateinit var loading: Loading
   private lateinit var move: Intent
+  private lateinit var username: EditText
+  private lateinit var password: EditText
   private lateinit var loginButton: Button
   private lateinit var textViewRegister: TextView
 
@@ -24,6 +31,8 @@ class LoginActivity : AppCompatActivity() {
 
     loginButton = findViewById(R.id.buttonLogin)
     textViewRegister = findViewById(R.id.textViewRegister)
+    username = findViewById(R.id.editTextUsername)
+    password = findViewById(R.id.editTextPassword)
 
     loginButton.setOnClickListener {
       loading.openDialog()
@@ -40,6 +49,30 @@ class LoginActivity : AppCompatActivity() {
         move = Intent(applicationContext, RegisterActivity::class.java)
         startActivity(move)
         loading.closeDialog()
+      }
+    }
+  }
+
+  private fun doLogin(){
+    val body = FormBody.Builder()
+    body.addEncoded("username", username.text.toString())
+    body.addEncoded("password", password.text.toString())
+    Timer().schedule(100) {
+      val result: JSONObject = WebController.Post("login", "", body).call()
+      if (result.getInt("code") == 200) {
+        runOnUiThread {
+          Toast.makeText(applicationContext, result.getJSONObject("data").getString("message"), Toast.LENGTH_SHORT).show()
+          loading.closeDialog()
+          move = Intent(applicationContext, NavigationActivity::class.java)
+          startActivity(move)
+          loading.closeDialog()
+          finish()
+        }
+      } else {
+        runOnUiThread {
+          Toast.makeText(applicationContext, result.getJSONObject("data").getString("message"), Toast.LENGTH_SHORT).show()
+          loading.closeDialog()
+        }
       }
     }
   }
