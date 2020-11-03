abdea836d18f98378db8d9d62f88c0b5f3a518c5
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-03 15:15:35 +0700

    Update DataUser.kt

--- app/src/main/java/info/bugnode/background/DataUser.kt
+++ app/src/main/java/info/bugnode/background/DataUser.kt
@@ -3,6 +3,7 @@ package info.bugnode.background
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import info.bugnode.BuildConfig
 import info.bugnode.controller.WebController
@@ -42,6 +43,7 @@ class DataUser : Service() {
               break
             }
             json = WebController.Get("user.show", user.getString("token")).call()
+            Log.i("response", json.toString())
             if (json.getInt("code") == 200) {
               user.setString("username", json.getJSONObject("data").getString("username"))
               user.setString("cookie", json.getJSONObject("data").getString("sessionKey"))
